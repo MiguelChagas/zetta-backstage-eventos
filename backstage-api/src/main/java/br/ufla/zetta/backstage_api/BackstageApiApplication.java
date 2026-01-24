1b84485e0bce3c7feba097cbe0008bcdd2dec800
MiguelChagas <108590753+MiguelChagas@users.noreply.github.com>
2026-01-24 17:02:37 -0300

    chore: configura os passos iniciais do banco de dados e do projeto

---
+++ backstage-api/src/main/java/br/ufla/zetta/backstage_api/BackstageApiApplication.java
@@ -0,0 +1,13 @@
+package br.ufla.zetta.backstage_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackstageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackstageApiApplication.class, args);
+	}
+
+}
